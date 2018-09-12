98a30f8da4d81b3982e15e7c79e88360eb3d7cb3
Mark Kuczmarski <markkuczmarski@gmail.com>
2018-09-12 10:05:44 +0900

    Break out the renderer

---
+++ markact.js
@@ -0,0 +1,41 @@
+// A better React and utils -- MarkAct.js
+
+function h(nodeName, attributes, ...args) {
+    let children = args.length ? [].concat(...args) : null;
+    return {
+        nodeName,
+        attributes,
+        children
+    };
+}
+
+function fastRender(prev, vnode) {
+    if (JSON.stringify(prev) === JSON.stringify(vnode)) return;
+    if (vnode.split) return document.createTextNode(vnode);
+    let n = document.createElement(vnode.nodeName);
+    let as = vnode.attributes || {};
+    for (let k in as) n.setAttribute(k, as[k]);
+    (vnode.children || []).map(c => n.appendChild(render(c)));
+    return n;
+}
+
+function render(vnode) {
+    if (vnode.split) return document.createTextNode(vnode);
+    let n = document.createElement(vnode.nodeName);
+    let as = vnode.attributes || {};
+    for (let k in as) n.setAttribute(k, as[k]);
+    (vnode.children || []).map(c => n.appendChild(render(c)));
+    return n;
+}
+
+function get(url, success) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var resp = request.responseText;
+            success(resp);
+        }
+    };
+    request.send();
+}
