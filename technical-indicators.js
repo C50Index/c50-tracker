3ecf2211c433879723d46dd4db0610ae01228902
Mark Kuczmarski <markkuczmarski@gmail.com>
2018-11-20 08:25:39 +0900

    200 day moving average buy or sell

--- technical-indicators.js
+++ technical-indicators.js
@@ -1,7 +1,21 @@
+// {name: "Two Hundred Day Moving Average", value: number, signal: "buy" | "sell" | "hold"}
+
+// Sum up all the close prices divide by 200
 function twoHundredDayMovingAverage(rows) {
   let twoHundredDaysAgo = moment().subtract(200,'days').unix();
+  let lastPrice = null;
 
-  return rows.reduce((acc, curr) => {
-    return acc + (Number(curr['time_unix'] >= twoHundredDaysAgo)) ? Number(curr['close']) : 0
-  } , 0) / 200;
+  let twoHundredDaySum = rows.reduce((acc, curr) => {
+    if (!!curr['close'] && curr['time_unix'] && Number(curr['time_unix']) >  twoHundredDaysAgo) {
+      lastPrice = Number(curr['close']);
+      return acc + Number(curr['close']);
+    } else {
+      return acc;
+    }
+  });
+
+  let result = twoHundredDaySum / 200;
+  return {name: "Two Hundred Day Moving Average",
+          value: result,
+          signal:  result > lastPrice ? "buy" : "sell"}
 }
